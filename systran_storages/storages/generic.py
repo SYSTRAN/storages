3ad0b85635ec4436730dbd9a061fdf500b5eb3b6
Dang Chuan Nguyen <dang-chuan.nguyen@systrangroup.com>
2020-11-06 11:53:27 +0100

    try to check integrity first to avoid request to remote storage if the local folder is already good

--- systran_storages/storages/generic.py
+++ systran_storages/storages/generic.py
@@ -107,6 +107,8 @@ class Storage(object):
         if not os.path.isdir(local_path):
             os.makedirs(local_path)
         with lock(local_path):
+            if check_integrity_fn is not None and check_integrity_fn(local_path):
+                return
             allfiles = {}
             for root, dirs, files in os.walk(local_path):
                 if os.path.basename(root) == _META_SUBDIR:
